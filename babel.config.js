eb96fcafc0aff347376e30f815ffbac353996000
Daniel Mejia <danielmejia@lmejia-mac.local>
2020-05-24 16:57:19 -0500

    Setup
    - Configure typescript model.
    - Add Airbnb rules for eslint.
    - Configure the dependencies
    - Configure the local  commands
    - Configure the npm publish commands

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,31 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        root: ['./src'],
+        alias: {
+          '@src': './dist',
+        },
+      },
+    ],
+    'jest-hoist',
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+  ],
 };
--- jest/setup.js
+++ jest/setup.js
@@ -2,6 +2,9 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+// Mocks
+import './react-native.mock';
+
 /**
  * @file setup.js
  * @author Daniel Mejia.
